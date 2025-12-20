4ed0ff6c29bb6f76372db3592417d532e9f06cd7
Honchar Denys <crackeraki@gmail.com>
2025-12-21 00:12:13 +0200

    refactoring regions

--- src/app/pages/public/regions/regions.component.ts
+++ src/app/pages/public/regions/regions.component.ts
@@ -6,6 +6,7 @@ import {
 	OnInit,
 	signal,
 } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BurgerComponent } from 'src/app/icons/burger/burger.component';
 import { UserPreviewComponent } from 'src/app/modules/user/components/user-preview/user-preview.component';
 import { UamapComponent } from 'src/app/page-components/uamap/uamap.component';
@@ -14,12 +15,40 @@ import { UamapComponent } from 'src/app/page-components/uamap/uamap.component';
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	templateUrl: './regions.component.html',
 	styleUrls: ['./regions.component.scss'],
-	imports: [UserPreviewComponent, BurgerComponent, UamapComponent],
+	imports: [CommonModule, UserPreviewComponent, BurgerComponent, UamapComponent],
 })
 export class RegionsComponent implements OnInit {
 	private _cdr = inject(ChangeDetectorRef);
 
 	isMenuOpen = signal(false);
+	readonly regions: string[] = [
+		'Cherkasy',
+		'Chernihiv',
+		'Chernivtsi',
+		'Dnipropetrovsk',
+		'Donetsk',
+		'Ivano-Frankivsk',
+		'Kharkiv',
+		'Kherson',
+		'Khmelnytskyi',
+		'Kirovohrad',
+		'Kyiv',
+		'Kyiv City',
+		'Luhansk',
+		'Lviv',
+		'Mykolaiv',
+		'Odesa',
+		'Poltava',
+		'Rivne',
+		'Sumy',
+		'Ternopil',
+		'Vinnytsia',
+		'Volyn',
+		'Zakarpattia',
+		'Zaporizhzhia',
+		'Zhytomyr',
+		'Crimea',
+	];
 
 	ngOnInit() {
 		this._cdr.detectChanges();
